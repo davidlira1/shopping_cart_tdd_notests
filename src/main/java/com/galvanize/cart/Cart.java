ede700d04e7ac8eb2c26280d39cab0260ae7444b
davidlira1 <liradavid100@gmail.com>
2021-04-26 11:04:48 -0700

    completed first test: createShoppingCart_initializedEmpty

--- src/main/java/com/galvanize/cart/Cart.java
+++ src/main/java/com/galvanize/cart/Cart.java
@@ -1,4 +1,12 @@
 package com.galvanize.cart;
 
+import java.util.ArrayList;
+
 public class Cart {
+
+    public ArrayList<Item> shoppingCart;
+
+    public Cart() {
+        shoppingCart = new ArrayList<Item>();
+    }
 }
--- src/test/java/com/galvanize/cart/CartTest.java
+++ src/test/java/com/galvanize/cart/CartTest.java
@@ -6,9 +6,22 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CartTest {
 
+    @Test
+    public void createAShoppingCart_initializedEmpty () {
+        //SETUP
+        Cart cart = new Cart();
+        int expected = 0;
+
+        //EXECUTION
+        int actual = cart.shoppingCart.size();
+
+        //ASSERT
+        assertEquals(expected, actual, "Expect cart to be empty when cart is initialized");
+    }
 
 }
