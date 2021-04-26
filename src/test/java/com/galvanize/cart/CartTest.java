89b7834b50c0a496adaaa882c3553da518622ebd
yvonn <yvonnersz@gmail.com>
2021-04-26 13:39:04 -0700

    Wrote createAnItem_initializedWithPrice()

--- src/test/java/com/galvanize/cart/CartTest.java
+++ src/test/java/com/galvanize/cart/CartTest.java
@@ -10,41 +10,55 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CartTest {
 
+//    @Test
+//    public void createAShoppingCart_doesShoppingCartExist() {
+//        //SETUP
+//        Cart cart = new Cart();
+//        ArrayList<Item> expected = new ArrayList<Item>();
+//
+//        //EXECUTION
+//        ArrayList<Item> actual = cart.shoppingCart;
+//
+//        //ASSERT
+//        assertEquals(actual, expected, "shopping cart exist");
+//    }
+//
+//    @Test
+//    public void createAShoppingCart_initializedEmpty () {
+//        //SETUP
+//        Cart cart = new Cart();
+//        int expected = 0;
+//
+//        //EXECUTION
+//        int actual = cart.shoppingCart.size();
+//
+//        //ASSERT
+//        assertEquals(expected, actual, "Expect cart to be empty when cart is initialized");
+//    }
+
+//    @Test
+//    public void createAnItem_doesPriceExist() {
+//        //SETUP
+//        Item item = new Item(9.75);
+//
+//        //EXECUTION
+////        boolean actual = Double.class.isInstance(item.price);
+//        String actual = item.price.getType();
+//
+//        //ASSERT
+//        assertTrue(actual, "field of price for item exists");
+//    }
+
     @Test
-    public void createAShoppingCart_doesShoppingCartExist() {
+    public void createAnItem_initializedWithPrice() {
         //SETUP
-        Cart cart = new Cart();
-        ArrayList<Item> expected = new ArrayList<Item>();
+        Item item = new Item(5.25);
+        double expected = 5.25;
 
         //EXECUTION
-        ArrayList<Item> actual = cart.shoppingCart;
+        double actual = item.getPrice();
 
         //ASSERT
-        assertEquals(actual, expected, "shopping cart exist");
-    }
-
-    @Test
-    public void createAShoppingCart_initializedEmpty () {
-        //SETUP
-        Cart cart = new Cart();
-        int expected = 0;
-
-        //EXECUTION
-        int actual = cart.shoppingCart.size();
-
-        //ASSERT
-        assertEquals(expected, actual, "Expect cart to be empty when cart is initialized");
-    }
-
-    @Test
-    public void createAnItem_doesPriceExist() {
-        //SETUP
-        Item item = new Item();
-
-        //EXECUTION
-        boolean actual = item.price instanceof double;
-
-        //ASSERT
-        assertTrue(actual, "field of price for item exists");
+        assertEquals(expected, actual, "item is initialized with price");
     }
 }
